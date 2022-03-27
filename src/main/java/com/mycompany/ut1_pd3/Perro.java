0f656b675e4cb194311b20822f9efe5ce1391e9a
Belen <maria.peirano@correo.ucu.edu.uy>
2022-03-27 12:48:43 -0300

    maquina a

---
+++ src/main/java/com/mycompany/ut1_pd3/Perro.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.ut1_pd3;
+
+/**
+ *
+ * @author belu_
+ */
+public class Perro {
+    private String nombre;
+    
+    public Perro(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void ladrar(){
+        System.out.println("wof");
+    }
+    public boolean respondeA(String unNombre){
+        return this.nombre.equals(unNombre);
+    }
+}
